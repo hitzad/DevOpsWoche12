7f5b081913f3d0f975b52605f677806820a498e9
hitzad <hitzadr1@students.zhaw.ch>
2024-05-17 23:25:52 +0200

    jenkins builds zusammensetzen

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Der Weg ist das Ziel, bald am Ende!');
+res.send('Der Weg ist das Ziel, bald am Ende! Habe ich gedacht... one more job');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
