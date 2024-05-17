a77396b7fe9f7c08ec5bd0ea4cda13cb74f1cda2
hitzad <hitzadr1@students.zhaw.ch>
2024-05-17 23:31:32 +0200

    Git und Jenkins automation

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Der Weg ist das Ziel, bald am Ende! Habe ich gedacht... one more job');
+res.send('Der Weg ist das Ziel, bald am Ende! Habe ich gedacht... one more job, falsch gedacht, final job');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
