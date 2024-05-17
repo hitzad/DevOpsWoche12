906751ce8b3c8acd2c90657d8c812681f6fcf902
hitzad <hitzadr1@students.zhaw.ch>
2024-05-17 23:18:11 +0200

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('DevOpsWoche 12 noch nie war es schöner !');
+res.send('Der Weg ist das Ziel, bald am Ende!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
